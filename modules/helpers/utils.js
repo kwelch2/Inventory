9f3c9175c71354054393b265d8744effdca09f5d
kwelch2 <kwelch@gemfireems.org>
2025-11-17 16:14:39 -0700

    d

--- modules/helpers/utils.js
+++ modules/helpers/utils.js
@@ -1,19 +1,16 @@
-// modules/helpers/utils.js
-import { $ } from "./helpers/utils.js";
-
-export const $ = (id) => document.getElementById(id);
-
-export function escapeHtml(str = "") {
-  return String(str).replace(/[&<>"']/g, m => ({
-    "&": "&amp;",
-    "<": "&lt;",
-    ">": "&gt;",
-    '"': "&quot;",
-    "'": "&#39;",
-  }[m]));
+// Lightweight DOM selector helper
+export function $(selector) {
+    return document.querySelector(selector);
 }
 
-export function dateStamp() {
-  const d = new Date();
-  return `${d.getMonth() + 1}-${d.getDate()}-${d.getFullYear().toString().slice(-2)}`;
+// Selector for multiple elements (optional but useful)
+export function $all(selector) {
+    return document.querySelectorAll(selector);
+}
+
+// Escape HTML to prevent accidental injection in UI text
+export function escapeHtml(str = "") {
+    return String(str).replace(/[&<>"']/g, m => (
+        { "&": "&amp;", "<": "&lt;", ">": "&gt;", '"': "&quot;", "'": "&#39;" }[m]
+    ));
 }
