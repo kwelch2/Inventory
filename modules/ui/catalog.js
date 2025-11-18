3bf149a0a193dcd00da894d644be64ee430e98cd
kwelch2 <kwelch@gemfireems.org>
2025-11-17 19:27:22 -0700

    l

--- modules/ui/catalog.js
+++ modules/ui/catalog.js
@@ -1,6 +1,6 @@
 // modules/ui/catalog.js
 import { state } from "../state.js";
-import { $, eescapeHtml, exportToCsv, downloadCsv} from "../helpers/utils.js";
+import { $, escapeHtml, exportToCsv, downloadCsv} from "../helpers/utils.js";
 import { openCatalogModal, openPricingModal } from "./modals.js"; // <-- Removed openLabelModal
 
 export function renderCatalog() {
